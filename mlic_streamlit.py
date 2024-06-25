b85c5e3c70edfe8a32915eee578e347cc9927a18
davidmakoyo <makoyo.david@me.com>
2024-06-24 21:50:24 -0400

    streamlit app

---
+++ mlic_streamlit.py
@@ -0,0 +1,94 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import streamlit as st
+import tensorflow as tf
+
+from PIL import Image
+
+def main():
+    st.title('Machine Learning Image Classifier')
+    st.write('Upload any image that fits into one of the available categories and see if the model can classify it correctly.')
+
+    st.subheader('Categories')
+    st.write('airplane, automobile, bird, cat, deer, dog, frog, horse, ship, truck')
+
+    file = st.file_uploader('Upload an image', type=['jpg', 'jpeg', 'png'], key='image')
+    if file:
+        image = Image.open(file)
+        st.image(image, caption='Uploaded Image', use_column_width=True)
+
+        # pre-process the image, load the model, and make a prediction
+        resized_image = image.resize((32, 32))
+        # need values between 0 and 1
+        img_array = np.array(resized_image) / 255
+        img_array = img_array.reshape((1, 32, 32, 3))
+
+        model = tf.keras.models.load_model('cifar10_model.h5')
+
+        predictions = model.predict(img_array)
+        cifar10_classes = ['airplane', 'automobile', 'bird', 'cat', 'deer', 'dog', 'frog', 'horse', 'ship', 'truck']
+
+        fig, ax = plt.subplots()
+        # we want to display not only the highest value but also the confidence of the model
+        y_pos = np.arange((len(cifar10_classes)))
+        ax.barh(y_pos, predictions[0], align='center')
+        ax.set_yticks(y_pos)
+        ax.set_yticklabels(cifar10_classes)
+        ax.invert_yaxis()
+        ax.set_xlabel('Confidence')
+        ax.set_title('Predictions')
+
+        st.pyplot(fig)
+    else:
+        st.text('You have not uploaded an image yet.')
+    
+    st.markdown("### [GitHub](https://github.com/davidmakoyo/Machine-Learning-Image-Classifier)")
+
+if __name__ == '__main__':
+    main()
+
+#     st.set_page_config(page_title="Machine Learning Image Classifier", page_icon=":camera:", layout="wide")
+    
+#     st.title('Machine Learning Image Classifier')
+#     st.write('Upload any image that fits into one of the available categories and see if the model can classify it correctly.')
+
+#     # Display the available categories
+#     st.subheader("Available Categories")
+#     st.write(", ".join(CIFAR10_CLASSES))
+    
+#     # File uploader
+#     file = st.file_uploader('Upload an image', type=['jpg', 'jpeg', 'png'], key='image')
+#     if file:
+#         image = Image.open(file)
+#         st.image(image, caption='Uploaded Image', use_column_width=True)
+
+#         # Pre-process the image, load the model, and make a prediction
+#         resized_image = image.resize((32, 32))
+#         img_array = np.array(resized_image) / 255.0
+#         img_array = img_array.reshape((1, 32, 32, 3))
+
+#         # Load the model
+#         model = tf.keras.models.load_model('cifar10_model.h5')
+
+#         # Make predictions
+#         predictions = model.predict(img_array)
+        
+#         # Plot the predictions
+#         fig, ax = plt.subplots()
+#         y_pos = np.arange(len(CIFAR10_CLASSES))
+#         ax.barh(y_pos, predictions[0], align='center')
+#         ax.set_yticks(y_pos)
+#         ax.set_yticklabels(CIFAR10_CLASSES)
+#         ax.invert_yaxis()
+#         ax.set_xlabel('Confidence')
+#         ax.set_title('Model Predictions')
+        
+#         # Display the plot
+#         st.pyplot(fig)
+#     else:
+#         st.text('You have not uploaded an image yet.')
+    
+#     # Add a link to the GitHub repository
+#     st.markdown("### [View on GitHub](https://github.com/davidmakoyo)")
+
+
